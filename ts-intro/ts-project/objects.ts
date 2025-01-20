740369dfc8c45af3a6255631e820c1774028bf7a
Cogger101 <seandcoughlan@hotmail.co.uk>
2025-01-20 13:25:16 +0000

    Code along for TS objects

--- ts-intro/ts-project/objects.ts
+++ ts-intro/ts-project/objects.ts
@@ -1,49 +1,3 @@
-import "./style.css";
-
-// INFERED TYPES
-
-const firstName = "Sean";
-console.log(typeof firstName);
-
-// EXPLICIT TYPING
-
-const myFaveNum: number = 8000;
-
-let isBoolean = true;
-
-// isBoolean = 2;  ---- typescrpit will notify of error for incorrect data types
-
-//declaring Explicit tpyes
-
-const temps = [12, 32, 23];
-
-const info = temps.map((num) => {
-    return `It is ${num}c`;
-}) as string[];
-
-const info2 = (<string[]>temps.map((num) => {
-    return `It is ${num}c`;
-})) as string[];
-
-const info3: string[] = temps.map((num) => {
-    return `It is ${num}c`;
-});
-
-// Challenge 1
-// Create a function that will generate an array of five random numbers between 1 and n.
-
-const randomNums = (n: number) => {
-    const numbers: number[] = [];
-    for (let i = 0; i < 5; i++) {
-        numbers.push(Math.floor(Math.random() * n));
-    }
-    return numbers;
-};
-
-// console.log(randomNums(20));
-
-// ------------------------------------------------------------------------------------------------
-
 //TS OBJECTS
 // What are objects?
 // How can we create objects?
@@ -131,13 +85,5 @@ const garfield: Cat = {
 };
 
 console.log("Garfield =>", garfield);
-
-// Using the spread operator - copies all the key value pairs inside the new object
-
-const newCat = { ...garfield };
-console.log(newCat);
-newCat.isAnnoying = false;
-//  newCat.name = 'Jonny'; does not follow the Animal / Cat interface
-console.log("newCat", newCat);
 
 // JSON = JavaScript Object Notation
