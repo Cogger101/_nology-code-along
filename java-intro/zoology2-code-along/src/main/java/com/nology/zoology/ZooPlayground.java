2c040343020f953b218319aa523f6e5b9ac7e5db
Cogger101 <seandcoughlan@hotmail.co.uk>
2025-02-05 12:34:35 +0000

    randomly generating IDs, age and names for each animals

--- java-intro/zoology2-code-along/src/main/java/com/nology/zoology/ZooPlayground.java
+++ java-intro/zoology2-code-along/src/main/java/com/nology/zoology/ZooPlayground.java
@@ -1,12 +1,16 @@
 package com.nology.zoology;
 
-import com.nology.zoology.animal.Crocodile;
-import com.nology.zoology.animal.Lion;
-import com.nology.zoology.animal.Llama;
-import com.nology.zoology.animal.Tiger;
+import com.nology.zoology.animal.*;
+import com.nology.zoology.command.UserCommandRunner;
+import com.nology.zoology.user.User;
 import com.nology.zoology.zoo.Zoo;
 
+import java.util.Random;
+
+import static com.nology.zoology.animal.AnimalUtils.*; //importing all methods from a class
+
 public class ZooPlayground {
+    public static Random random = new Random();
 
     public static void main(String[] args) {
         System.out.println("HELLO");
@@ -16,42 +20,54 @@ public class ZooPlayground {
 //        Encapsulation
 //        Polymorphism pt.1
 
-        Lion simba = new Lion(1, "Simba", 2);
-        Llama kuzco = new Llama(2, "Kuzco", 5);
-        Tiger tony = new Tiger(3, "Tony", 23);
-        Crocodile krok = new Crocodile(4, "Krok", 7);
+//        Lion simba = new Lion(nextUniqueId(), "Simba", 2, true);
+//        Llama kuzco = new Llama(2, "Kuzco", 5);
+//        Tiger tony = new Tiger(3, "Tony", 23);
+//        Crocodile krok = new Crocodile(4, "Krok", 7);
 
-        System.out.println(simba.getInformation());
-        System.out.println(kuzco.getInformation());
-        System.out.println(tony.getInformation());
-        System.out.println(krok.getInformation());
 
-        String[] movies = {"Lion King", "Emperor's new groove", "The Jungle " +
-                "Books"};
-
-        System.out.println(movies.length);
-
-        for (String movie : movies) {
-            System.out.println(movie);
-        }
 
         Zoo myZoo = new Zoo();
 
-        for(int i = 0; i < 3; i++) {
-            myZoo.addAnimal(tony);
+//        Lion randomLion = new Lion(nextUniqueId(), generateName(), generateRandomAge(), true);
+//        System.out.println(randomLion.getName());
+//        System.out.println(randomLion.getAge());
+
+
+        //Random
+        for (int i = 0; i <13; i++){
+            switch (random.nextInt(4)){
+                case 0:
+                    myZoo.addAnimal(new Lion(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge(), true));
+                    break;
+                case 1:
+                    myZoo.addAnimal(new Tiger(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge()));
+                    break;
+                case 2:
+                    myZoo.addAnimal(new Llama(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge()));
+                    break;
+                case 3:
+                    myZoo.addAnimal(new Crocodile(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge()));
+                    break;
+            }
         }
 
-        for(int i = 0; i < 4; i++) {
-            myZoo.addAnimal(kuzco);
-        }
-
-        for(int i = 0; i < 5; i++) {
-            myZoo.addAnimal(krok);
-        }
+//        for (int i = 0; i < 3; i++) {
+//            myZoo.addAnimal(new Lion(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge(), true));
+//        }
+//
+//        for (int i = 0; i < 4; i++) {
+//            myZoo.addAnimal(new Tiger(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge()));
+//        }
+//
+//        for (int i = 0; i < 5; i++) {
+//            myZoo.addAnimal(new Llama(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge()));
+//        }
 
         System.out.println(myZoo.getAnimalCount());
-        myZoo.listAnimals();
 
+        UserCommandRunner userCommandRunner = new UserCommandRunner(myZoo);
+        userCommandRunner.runCommands();
 
     }
 
