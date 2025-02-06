773ae8fdfa4b8e548ff234a4b5a29aaecafb072e
Cogger101 <seandcoughlan@hotmail.co.uk>
2025-02-06 11:35:42 +0000

    adding scanner for user input to generate animals in zoo or individual amount of each animal

--- java-intro/zoology2-code-along/src/main/java/com/nology/zoology/ZooPlayground.java
+++ java-intro/zoology2-code-along/src/main/java/com/nology/zoology/ZooPlayground.java
@@ -5,7 +5,9 @@ import com.nology.zoology.command.UserCommandRunner;
 import com.nology.zoology.user.User;
 import com.nology.zoology.zoo.Zoo;
 
+import java.sql.SQLOutput;
 import java.util.Random;
+import java.util.Scanner;
 
 import static com.nology.zoology.animal.AnimalUtils.*; //importing all methods from a class
 
@@ -14,7 +16,7 @@ public class ZooPlayground {
 
     public static void main(String[] args) {
         System.out.println("HELLO");
-
+        Scanner scanner = new Scanner(System.in);
 //        Java intro to basics (variables, types, control flow, loops, arrays)
 //        Abstraction
 //        Encapsulation
@@ -24,33 +26,56 @@ public class ZooPlayground {
 //        Llama kuzco = new Llama(2, "Kuzco", 5);
 //        Tiger tony = new Tiger(3, "Tony", 23);
 //        Crocodile krok = new Crocodile(4, "Krok", 7);
-
-
-
         Zoo myZoo = new Zoo();
 
+        System.out.println("Would you like to choose a number of each animal - y = Yes, n = No: \n");
+        String yesOrNo = scanner.nextLine();
+
+        if (yesOrNo.equals("y")){
+
+            System.out.println("Enter the number of Lions: \n");
+            int numberOfLions = scanner.nextInt();
+            for (int i = 0; i < numberOfLions; i++) {
+                myZoo.addAnimal(new Lion(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge(), true));
+            }
+
+            System.out.println("Enter the number of Tiger: \n");
+            int numberOfTiger = scanner.nextInt();
+            for (int i = 0; i < numberOfTiger; i++) {
+                myZoo.addAnimal(new Tiger(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge()));
+            }
+
+            System.out.println("Enter the number of Llama: \n");
+            int numberOfLlama = scanner.nextInt();
+            for (int i = 0; i < numberOfLlama; i++) {
+                myZoo.addAnimal(new Llama(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge()));
+            }
+        } else {
+
+            System.out.println("Enter your number of animals: \n");
+            int numberOfAnimals = scanner.nextInt();
+
+            //Random
+            for (int i = 0; i < numberOfAnimals; i++) {
+                switch (random.nextInt(4)) {
+                    case 0:
+                        myZoo.addAnimal(new Lion(nextUniqueId(), generateName(), generateRandomAge(), true));
+                        break;
+                    case 1:
+                        myZoo.addAnimal(new Tiger(nextUniqueId(), generateName(), generateRandomAge()));
+                        break;
+                    case 2:
+                        myZoo.addAnimal(new Llama(nextUniqueId(), generateName(), generateRandomAge()));
+                        break;
+                    case 3:
+                        myZoo.addAnimal(new Crocodile(nextUniqueId(), generateName(), generateRandomAge()));
+                        break;
+                }
+            }
+        }
 //        Lion randomLion = new Lion(nextUniqueId(), generateName(), generateRandomAge(), true);
 //        System.out.println(randomLion.getName());
 //        System.out.println(randomLion.getAge());
-
-
-        //Random
-        for (int i = 0; i <13; i++){
-            switch (random.nextInt(4)){
-                case 0:
-                    myZoo.addAnimal(new Lion(nextUniqueId(), generateName(), generateRandomAge(), true));
-                    break;
-                case 1:
-                    myZoo.addAnimal(new Tiger(nextUniqueId(), generateName(), generateRandomAge()));
-                    break;
-                case 2:
-                    myZoo.addAnimal(new Llama(nextUniqueId(), generateName(), generateRandomAge()));
-                    break;
-                case 3:
-                    myZoo.addAnimal(new Crocodile(nextUniqueId(), generateName(), generateRandomAge()));
-                    break;
-            }
-        }
 
 //        for (int i = 0; i < 3; i++) {
 //            myZoo.addAnimal(new Lion(AnimalUtils.nextUniqueId(), generateName(), generateRandomAge(), true));
