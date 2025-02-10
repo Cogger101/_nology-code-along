6ed1576f952b0628de0fd636a6fd8d5e672941d0
Cogger101 <seandcoughlan@hotmail.co.uk>
2025-02-10 10:49:41 +0000

    adding interfaces code along

---
+++ java/interfaces/src/main/java/Duck.java
@@ -0,0 +1,14 @@
+public class Duck extends Animal implements CanFly, CanMakeSound {
+    @Override
+    public AnimalType getType() {
+        return AnimalType.duck;
+    }
+    @Override
+    public void fly() {
+        System.out.println("Flap flap");
+    }
+    @Override
+    public void makeSound() {
+        System.out.println("Quack quack!");
+    }
+}
