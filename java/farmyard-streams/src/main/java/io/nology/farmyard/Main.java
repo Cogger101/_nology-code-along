68d557a69d2b75008cb3173c722266043b5c7f1e
Cogger101 <seandcoughlan@hotmail.co.uk>
2025-02-13 10:44:17 +0000

    adding lambda functions to sort animals

--- java/farmyard-streams/src/main/java/io/nology/farmyard/Main.java
+++ java/farmyard-streams/src/main/java/io/nology/farmyard/Main.java
@@ -7,9 +7,29 @@ import java.util.List;
 public class Main {
 
     private static FarmYard farmYard = new FarmYard();
+    static List<Animal> animals = new ArrayList<>(farmYard.getAnimals());
 
     public static void main(String[] args) {
+////        How to print out each animal
+//        for (Animal animal:animals){
+//            System.out.println(animal);
+//        }
+////        How would we sort the animals by name?
+//        animals.sort(new SortByAnimalName());
+//        System.out.println("After sorting");
+//        for  (Animal animal: animals){
+//            System.out.println(animal);
+//        }
 
+
+        animals.forEach(animal -> {
+            System.out.println(animal.getName());
+            System.out.println(animal.getAge());
+        });
+
+        animals.sort((a, b) -> a.getAge() - b.getAge());
+        System.out.println("After sorting by age");
+        animals.forEach(System.out::println);
     }
 
 }
